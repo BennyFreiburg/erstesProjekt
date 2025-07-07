239bdcebf4b1a5c14c4b2dcbddd6ba329ad09f9d
MD-43700668476\Student <b_malicke@hotmail.com>
2025-07-07 10:48:00 +0200

    mit pi^2/6

--- Test.py
+++ Test.py
@@ -1,6 +1,11 @@
 print('Hallo Freiburg')
 print('Das ist eine Veränderung')
 summe=0
+summe2=0
 for i in range(101):
     summe=summe+i
 print(summe)
+for i in range(100):
+    summe2=summe2+1/(i+1)**2
+    
+print(summe2)
